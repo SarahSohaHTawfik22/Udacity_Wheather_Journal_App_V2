a585867d7a0bbe7b0f7f246256759efd54733422
SarahSohaHTawfik22 <you@example.com>
2021-03-25 18:08:32 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+// Setup empty JS object to act as endpoint for all routes
+let projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Start up an instance of app
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 3335;
+
+const server = app.listen(port, listening);
+function listening(){
+    console.log(`server running on localhost:${port}`);
+}
+//route GET
+app.get('/Alldataa', geting_Weather);
+function geting_Weather(request, response) {
+  response.send(projectData);
+}
+//route POST
+app.post('/Weatheradding', addingg_Weather);
+function addingg_Weather(request,response){
+  projectData.feels = request.body.feels;
+  projectData.date = request.body.date;
+  projectData.temperature = request.body.temperature;
+
+    response.send(projectData);
+}
